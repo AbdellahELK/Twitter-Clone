b6913852497f968241b89c927a1298f58b107bf8
AbdellahELK <abdellahelkhald@gmail.com>
2025-02-02 20:40:21 +0100

    prepare my app for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,19 @@ import postRoutes from './routes/postRoutes.js'
 import notificationRoutes from './routes/notificationRoutes.js'
 import { connectToDb } from './config/db.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 import { v2 as cloudinary } from 'cloudinary'
 import cors from 'cors';
 
 const app = express()
 app.use(cors());
-app.use(express.json({limit:"5mb"}))
+app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
 dotenv.config()
 
+
+const __dirname = path.resolve()
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
@@ -29,7 +32,13 @@ app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/notifications", notificationRoutes)
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 const port = process.env.PORT
 app.listen(port, () => {
